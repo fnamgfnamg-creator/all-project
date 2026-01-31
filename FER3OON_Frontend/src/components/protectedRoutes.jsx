efd42fec38e7de1aac35a1fb58622d031e6e4585
fnamgfnamg-creator <fnamgfnamg@gmail.com>
2026-01-31 02:33:45 +0200

    Delete FER3OON_Frontend directory

--- FER3OON_Frontend/src/components/protectedRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { authService } from '../services/authService';
-
-export const ProtectedRoute = ({ children }) => {
-  if (!authService.isAuthenticated()) {
-    return <Navigate to="/" replace />;
-  }
-  return children;
-};
